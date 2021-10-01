7aad6e3461af16dc6349ea1b3f9603970a522148
Abe Hendricks <ahendricks@pcwa.net>
2021-10-01 07:12:26 -0700

    Rebuilt component

--- packages/pkg/dist/MatGeolocator/MatGeolocator.d.ts
+++ packages/pkg/dist/MatGeolocator/MatGeolocator.d.ts
@@ -1,10 +1,9 @@
 import { FabProps, CircularProgressProps } from '@mui/material';
 declare type Props = {
     onClick: (coords: GeolocationCoordinates) => void;
-    classes: any;
     timeout?: number;
     onGeolocateError?: (error: GeolocationPositionError) => void;
     circularProgressProps?: Partial<CircularProgressProps>;
 } & Partial<FabProps>;
-declare const MatGeoLocator: ({ classes, timeout, size, onClick, onGeolocateError, circularProgressProps, ...rest }: Props) => JSX.Element;
+declare const MatGeoLocator: ({ timeout, size, onClick, onGeolocateError, circularProgressProps, ...rest }: Props) => JSX.Element;
 export default MatGeoLocator;
